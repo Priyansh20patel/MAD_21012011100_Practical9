b681e8fe8dd5ded2b1b03c114af3b79fb7bfba2b
Priyansh <priyashpatel21@gnu.ac.in>
2023-09-28 10:21:41 +0530

    P-9

---
+++ app/src/main/java/com/priyanshpatel/mad_21012011100_practical9/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.priyanshpatel.mad_21012011100_practical9
+
+import android.graphics.drawable.AnimationDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var alarmanimation : AnimationDrawable
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val alarm : ImageView = findViewById(R.id.alarm)
+        alarm.setBackgroundResource(R.drawable.alarm_animation_list)
+        alarmanimation = alarm.background as AnimationDrawable
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            alarmanimation.start()
+        }
+        else {
+            alarmanimation.stop()
+        }
+    }
+}
---
+++ app/src/main/java/com/priyanshpatel/mad_21012011100_practical9/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.priyanshpatel.mad_21012011100_practical9
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
